4ae2ec3a35ab92b909129394ed6aaa2655bdf405
Ivan <ivan.turutin@gmail.com>
2021-12-22 20:55:02 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -10,18 +10,18 @@ public class Win {
         return false;
     }
 
-    private static boolean checkVert (int[][] board, int index) {
+    private static boolean checkVert(int[][] board, int index) {
         for (int i = 0; i < board.length; i++) {
-            if (board[i][index] != 1){
+            if (board[i][index] != 1) {
                 return false;
             }
         }
         return true;
     }
 
-    private static boolean checkHoriz (int[][] board, int index) {
+    private static boolean checkHoriz(int[][] board, int index) {
         for (int i = 0; i < board[index].length; i++) {
-            if (board[index][i] != 1){
+            if (board[index][i] != 1) {
                 return false;
             }
         }
