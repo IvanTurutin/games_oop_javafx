3dbeb9f39d079e5233982ee94050a7ba0046bd82
Ivan <ivan.turutin@gmail.com>
2022-01-12 11:27:22 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,7 +7,6 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-
 public class LogicTest {
     @Test
     public void whenCorrectMove()
